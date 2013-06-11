cbb667b142c891b731a8c52411d495e89404232e
zhou <zhou@zhoutekiMacBook-Air.local>
2013-06-11 16:01:13 +0800

    Initial Commit

---
+++ poj/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  poj
+//
+//  Created by zhou on 13-6-11.
+//  Copyright (c) 2013年 zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
