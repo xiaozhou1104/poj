f7442979c769bed2bdee8665c6d59c2e5d7f825c
xiaozhou1104 <583095503@qq.com>
2013-07-30 23:32:06 +0800

    1061

--- poj/poj1061.cpp
+++ poj/poj1061.cpp
@@ -1,55 +1,88 @@
-#include <iostream>
+#include <cstdio>
+
+void swap_two(long long int& x, long long int& y){
+	long long int temp = x;
+	x = y;
+	y = temp;
+};
+
+long long int Gcd(long long int a, long long int b){
+
+	if(b == 0)
+		return a;
+
+	return Gcd(b, a%b);
+
+}
+
+long long int exGcd(long long int a, long long int b, long long int& x, long long int& y){
+
+	long long int d = 0;
+	if(b == 0){
+		x = 1;
+		y = 0;
+		return a;
+	}
+	
+	long long int temp_x;
+	long long int temp_y; 
+	d = exGcd(b, a%b, temp_x, temp_y);
+	
+	x = temp_y;
+	y = temp_x - a / b * temp_y;
+
+	return d;
+}
+
 
 int main(){
 
-
 	long long int x,y,m,n,l;
-	while(scanf("%lld%lld%lld%lld%lld",&x,&y,&m,&n,&l)!=EOF){
-	//scanf("%lld%lld%lld%lld%lld",&x,&y,&m,&n,&l);
-
-	bool meet = false;
-	long long int count = 0;
-	if(m - n == 0){
-		printf("Impossible\n");	
+	scanf("%lld%lld%lld%lld%lld",&x,&y,&m,&n,&l);
+	bool meet = true;
+	if(m == n){
+		meet = false;
 	}
-	else if(m > n){
-		if(x < y && (y-x)%(m-n) == 0){
-			meet = true;
-			count = (y-x)/(m-n);
-		}
-		else{
-			for(int i=1; i<=m-n; i++){
-				if((y-x+i*l) % (m-n) == 0){
-					count = (y - x + i * l) / (m - n);
-					meet = true;
-					break;
-				}
+	else if(n > m){
+		swap_two(m,n);
+		swap_two(x,y);
+	}
+	long long int temp_gcd = Gcd(m-n, l);
+	if(((y-x) % temp_gcd) != 0)
+		meet = false;
+	if(meet == false)	
+		printf("Impossible\n");
+	else{
+		long long int exgcd_a = (m-n) / temp_gcd;
+		long long int exgcd_n = (y-x) / temp_gcd;
+		l = l / temp_gcd;
+
+		long long int exgcd_x,exgcd_y;
+		long long int exgdc_d = exGcd(exgcd_a,l,exgcd_x,exgcd_y);
+		exgcd_x *= exgcd_n;
+
+		if(exgcd_x < 0){
+			while(exgcd_x < 0){
+				exgcd_x += l;
 			}
 		}
-	}
-	else{
-		if(y < x && (x-y)%(n-m) == 0){
-			meet = true;
-			count = (x-y)/(n-m);
-		}
 		else{
-			for(int i=1; i<=n-m; i++){
-				if((x - y + i * l) % (n - m) == 0){
-					count = (x - y + i * l) / (n - m);
-					meet = true;
-					break;
-				}
-			}	
+			while(exgcd_x - l > 0){
+				exgcd_x -= l;
+			}
 		}
+
+		printf("%lld", exgcd_x);
+	
 	}
 
-	if(meet == true){
-		printf("%lld\n", count);
-	}
-	else{
-		printf("Impossible\n");	
-	}
-}
+	//long long int x1,x2,x3,x4;
+	//int x5;
+	//x1 = 11;
+	//x2 = 5;
+	//x5 = exGcd(x1,x2,x3,x4);
+
+	//printf("%lld\n%lld\n%d\n", x3,x4,x5);
 
 	return 0;
 }
